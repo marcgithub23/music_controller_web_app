fa064f1342054b2b9cc03d0608bc1cdb8d96127a
marcgithub23 <marc.dagohoy2@gmail.com>
2024-11-24 12:59:50 +0000

    Define generate unique code function

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -1,5 +1,19 @@
 from django.db import models
+import string
+import random
 
+
+def generate_unique_code():
+    length = 6
+
+    while True:
+        # Generate random code
+        code = ''.join(random.choices(string.ascii_uppercase, k=length))
+        # Check if code is unique
+        if Room.objects.filter(code=code).count() == 0:
+            break
+
+    return code
 
 
 # Create your models here.
